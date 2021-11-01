a4a26de4763aa4ce2c31e4abd35b705755662553
yacinebkt <yacinebakhti1mi@gmail.com>
2021-11-01 03:06:59 +0100

    hadle Form data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
+import Input from "./Components/Forms/Input/Input";
 
 function App() {
+  //  const [name, setName] = useState('')
+  // const userNameRef = useRef();
+  // const lastNameRef = useRef();
+  // const emailRef = useRef();
+  // const dateRef = useRef();
+
+  const [registerValues, setRegisterValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    // console.log(Object.fromEntries(data.entries()))
+  };
+
+  const onChangeRegisterValues = (e) => {
+    setRegisterValues({ ...registerValues, [e.target.name]: e.target.value });
+  };
+
+  console.log(registerValues)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <Input
+          placeholder="FirstName"
+          name="firstName"
+          type="text"
+          label="Frst Name"
+          value={registerValues.firstName}
+          onChange={onChangeRegisterValues}
+        />
+        <Input
+          placeholder="LastName"
+          name="lastName"
+          type="text"
+          label="Last Name"
+          value={registerValues.lastName}
+          onChange={onChangeRegisterValues}
+        />
+        <Input
+          placeholder="Email"
+          name="email"
+          type="text"
+          label="Email"
+          value={registerValues.email}
+          onChange={onChangeRegisterValues}
+        />
+        <Input
+          placeholder="birthday"
+          name="birthday"
+          type="date"
+          label="Birthday"
+          value={registerValues.data}
+          onChange={onChangeRegisterValues}
+        />
+        <Input
+          placeholder="password"
+          name="password"
+          type="password"
+          label="Password"
+          value={registerValues.password}
+          onChange={onChangeRegisterValues}
+        />
+        <Input
+          placeholder="confirm Password"
+          name="confirmPassword"
+          type="password"
+          label="Confirm Password"
+          value={registerValues.confirmPassword}
+          onChange={onChangeRegisterValues}
+        />
+        <button> Sumbit</button>
+      </form>
     </div>
   );
 }
---
+++ src/Components/Forms/Input/Input.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+export default function Input(props) {
+  return (
+    <div className="FormIn">
+      <label> {props.label}</label>
+      <input 
+        placeholder={props.placeholder} 
+        name={props.name}
+        type = {props.type}
+        value={props.value}
+        onChange={props.onChange}
+      
+      />
+      
+    </div>
+  );
+}
